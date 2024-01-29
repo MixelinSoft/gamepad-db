01b66ed0889f726398619bc55244cf624034c2be
MixelinSoft <mixelinsoft@gmail.com>
2024-01-29 22:13:33 +0200

    Start of Development

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <p>Testing</p>
+      <p className='test'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+        blandit feugiat urna, tincidunt feugiat turpis aliquam id. Morbi id
+        felis non est blandit viverra at sed dolor. Cras accumsan, arcu eu
+        gravida viverra, tellus augue scelerisque velit, in pellentesque risus
+        libero sed odio. Cras tempor congue mauris in ullamcorper. Aliquam erat
+        volutpat. Proin ac vehicula magna, non scelerisque metus. Suspendisse at
+        dapibus lectus, pharetra accumsan nisi. Aenean vulputate ipsum aliquam
+        suscipit efficitur. Cras diam nibh, dictum vel suscipit ac, mollis et
+        justo. Proin congue arcu quis ullamcorper sodales. Sed a arcu nec ligula
+        cursus accumsan. Maecenas eu lacus et est gravida gravida. Sed hendrerit
+        turpis quis congue ullamcorper. Vivamus vitae consequat orci.
+        Pellentesque sit amet felis ut dui imperdiet pharetra. Cras quis
+        ullamcorper nisi. Fusce at scelerisque neque, nec tempus enim. Cras et
+        venenatis est. Nulla sollicitudin sodales urna, vitae tristique tellus
+        vestibulum nec. Duis ac dignissim dolor. Cras hendrerit nisi est, et
+        tincidunt lacus efficitur quis. Sed eget arcu faucibus, posuere risus
+        non, iaculis justo. Pellentesque habitant morbi tristique senectus et
+        netus et malesuada fames ac turpis egestas. Aliquam nulla dolor, viverra
+        eget egestas quis, commodo laoreet nulla. Fusce a turpis tempor,
+        fermentum massa a, consectetur ligula. Nullam et magna vitae dui
+        dignissim imperdiet. Nullam suscipit ipsum tellus, sed fringilla velit
+        vestibulum ac. Pellentesque eget enim at leo aliquam blandit. Orci
+        varius natoque penatibus et magnis dis parturient montes, nascetur
+        ridiculus mus. Nam ipsum nunc, feugiat non sapien sit amet, feugiat
+        consectetur lorem. Nam gravida nec nisi id auctor. Ut cursus ultricies
+        eros et scelerisque. Sed nulla dolor, euismod quis pretium nec,
+        convallis ac nunc. Ut tempus lorem vitae nunc cursus vehicula. Aliquam
+        porta convallis interdum. Aliquam erat volutpat. In finibus, metus
+        fringilla vestibulum molestie, ex leo accumsan nisl, id commodo lorem
+        eros a turpis. Phasellus rutrum fermentum vulputate. Suspendisse commodo
+        scelerisque tellus, quis eleifend augue ornare eu. Fusce volutpat sapien
+        massa. Praesent porta felis ipsum, nec euismod purus elementum non.
+        Nullam scelerisque urna in ante mattis, in vestibulum felis tempus.
+        Etiam efficitur ullamcorper consequat. Nulla leo lectus, tincidunt ut
+        ultricies eget, suscipit in orci. Pellentesque eu sapien quis felis
+        consequat imperdiet. Cras sed lectus id tellus mattis condimentum non id
+        tellus. Suspendisse potenti. Morbi porta eu ante sed faucibus. Proin ut
+        dolor sed velit consequat faucibus ac et turpis. Cras dapibus nibh
+        convallis orci luctus, quis dignissim dui volutpat. Vestibulum egestas
+        turpis in est placerat, in elementum nunc placerat. Suspendisse mi orci,
+        convallis eu nulla sit amet, laoreet accumsan purus. Proin at semper
+        lacus. Donec quis pharetra justo. Donec aliquet dolor ultricies,
+        volutpat enim sit amet, malesuada erat. Mauris sit amet massa ex.
+        Aliquam id nisi tincidunt metus vulputate sodales. In varius ipsum quis
+        ipsum pulvinar tincidunt. Sed sed dui gravida, malesuada velit in, porta
+        mi. Aenean euismod magna leo, a vulputate ligula scelerisque eget. Nam
+        et ante sollicitudin, tempor turpis at, maximus purus.
+      </p>
     </div>
   );
 }
